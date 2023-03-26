7aecc9bc3bc9cdd0f9ce31e939af22a1943b752d
JATINGARG25 <116997476+JATINGARG25@users.noreply.github.com>
2023-03-26 10:28:56 +0530

    Add files via upload

---
+++ 01_variables.py
@@ -0,0 +1,22 @@
+a = '''Jatin is a very good boy.'''
+b = 3445
+c = 12.23
+d = True
+e = None
+A = 34   # Case-sensitive ... There is difference betwwen a and A..
+
+#Printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(A)
+
+#Printing the type of variables 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(A))
