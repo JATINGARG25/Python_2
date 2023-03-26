ebe77587c425c093986f35b059c4762b18b4ede7
JATINGARG25 <116997476+JATINGARG25@users.noreply.github.com>
2023-03-26 10:32:24 +0530

    Add files via upload

---
+++ 02_operators.py
@@ -0,0 +1,30 @@
+a = 3 
+b = 4
+
+#Arithmetic operator
+
+print("The value of 3+4 is",3+4)
+print("The value of 3+4 is",3-4)
+print("The value of 3+4 is",3*4)        # '*' Means Mutiply
+print("The value of 3+4 is",3/4)
+
+#Assignment operators
+
+a+=12
+a-=12
+a*=12
+a/=12
+print(a)
+
+#Comparision operator
+
+print(14==7)
+print(14!=7)
+
+#Logical operator
+
+bool1=True
+bool2=False
+print("The value of bool1 and bool2 is",(bool1 and bool2))
+print("The value of bool1 and bool2 is",(bool1 or bool2))
+print("The value of bool1 and bool2 is",(not bool2))
