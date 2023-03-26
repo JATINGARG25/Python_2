f3fb5e931f02d6ec26c66abac3b58f5fef3bf2e3
JATINGARG25 <116997476+JATINGARG25@users.noreply.github.com>
2023-03-26 10:32:53 +0530

    Add files via upload

---
+++ 03_timecasting.py
@@ -0,0 +1,6 @@
+a = "3434"
+a = int(a)   # Time casting(if possible)
+
+print(type(a))
+print(a)
+print(a+5)
---
+++ 04_inputfunction.py
@@ -0,0 +1,4 @@
+a = input("Enter your number: ")  # it will always give a string datatype
+print("The number enter is",a)
+a = int(a)         # Time casting of string into integer (if possible)
+print(type(a))
